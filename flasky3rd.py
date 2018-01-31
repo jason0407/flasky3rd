c9b086779072b0749496b8da358f108d432cd898
KayiSoft <wenzhengde@me.com>
2018-01-31 14:49:07 +0800

    增加session和url_for 处量刷新后表单重新提交的问题

--- flasky3rd.py
+++ flasky3rd.py
@@ -12,11 +12,11 @@ from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from datetime import datetime
 from flask_moment import Moment
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired
 #引入主要用于重定向，以解决刷新后需要表格重新提交
-from flask import sessions,url_for
+from flask import session,url_for
 
 
 app = Flask(__name__)
@@ -31,7 +31,7 @@ manager = Manager(app)
 moment = Moment(app)
 # 实例化以使用bootstrap
 
-class NameForm(Form):
+class NameForm(FlaskForm):
     # 原来书上使用的是Require现在已经废弃改成DataRequire前面导入的时候也相应修改
     name = StringField('姓名： ',validators=[DataRequired()])
     submit = SubmitField('提交：')
@@ -43,12 +43,13 @@ def index():
 # 这里要注意加入POST和GET否则点击后会报500错误
 @app.route('/wtf',methods=['POST','GET'])
 def wtf():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('wtf.html',form=form,name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('wtf'))
+        #用session改造刷新问题
+        #name = form.name.data
+    return render_template('wtf.html',form=form,name=session.get('name'))
 
 
 # 传递参数进去后在render_template里面要使用name=name
