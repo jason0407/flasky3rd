5c44e7cc535abc4da8ecca109ccecf610bf95f84
KayiSoft <wenzhengde@me.com>
2018-02-06 21:14:38 +0800

    9.3

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from . import db
 #加入密码散列 其中generate_password_hash用于生成散列密码，check_password_hash用于校对散列密码
 from werkzeug.security import generate_password_hash,check_password_hash
-from flask_login import UserMixin
+from flask_login import UserMixin,AnonymousUserMixin
 from . import login_manager
 from flask import current_app
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
@@ -16,12 +16,41 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean,default=False,index=True)
+    permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role')
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User':(Permission.FOLLOW |
+                    Permission.COMMENT |
+                    Permission.WRITE_ARTICLES,True),
+            'Moderator':(Permission.FOLLOW |
+                         Permission.COMMENT |
+                         Permission.WRITE_ARTICLES |
+                         Permission.MODERATE_COMMENTS,False),
+            'Administraotr':(0xff,False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name = r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
 
     # 返回一个可读性的字符串表示模型主要用于测试
     def __repr__(self):
         return '<Role %r>' % self.name
 
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMMENTS = 0x04
+    ADMINISTER = 0X80
 
 # 这里要注意需要增加一个UserMixin的一个参数用于标识用户状态，is_authenticated()表示用户已经登陆会返回True,is_active()表示是否允许登陆
 # is_anonymous()对普通用户必须返回False
@@ -35,6 +64,19 @@ class User(UserMixin,db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     confirmed = db.Column(db.Boolean,default=False)
 
+    def __init__(self,**kwargs):
+        super(User,self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == 'wenzhengde@qq.com':
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+
+    def can(self,permissions):
+        return self.role is not None and (self.role.permissions & permissions) ==permissions
+
+    def is_administrator(self):
+        return self.can(Permission.ADMINISTER)
 
     def generate_confirmation_token(self,expiration=3600):
         s = Serializer(current_app.config['SECRET_KEY'],expiration)
@@ -96,3 +138,11 @@ class User(UserMixin,db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    class AnonymousUser(AnonymousUserMixin):
+        def can(self,permissions):
+            return False
+
+        def is_administrator(self):
+            return False
+    login_manager.anonymous_user = AnonymousUser
