c6289c5ab21056fb0409156603ddfd096e4876b7
KayiSoft <wenzhengde@me.com>
2018-02-06 22:49:37 +0800

    完成10.1内容

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from flask_login import UserMixin,AnonymousUserMixin
 from . import login_manager
 from flask import current_app
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from datetime import datetime
 
 
 @login_manager.user_loader
@@ -63,6 +64,15 @@ class User(UserMixin,db.Model):
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     confirmed = db.Column(db.Boolean,default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(),default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(),default=datetime.utcnow)
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
 
     def __init__(self,**kwargs):
         super(User,self).__init__(**kwargs)
