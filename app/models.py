f017800cd4d291705e6d3aef23c8b2eb76029312
KayiSoft <wenzhengde@me.com>
2018-02-07 16:50:53 +0800

    完成第10章最后的内容，已经实现用户头像的显示。

--- app/models.py
+++ app/models.py
@@ -3,9 +3,10 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin,AnonymousUserMixin
 from . import login_manager
-from flask import current_app
+from flask import current_app,request
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from datetime import datetime
+import hashlib
 
 
 @login_manager.user_loader
@@ -69,6 +70,7 @@ class User(UserMixin,db.Model):
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(),default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(),default=datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
 
     def ping(self):
         self.last_seen = datetime.utcnow()
@@ -81,6 +83,8 @@ class User(UserMixin,db.Model):
                 self.role = Role.query.filter_by(permissions=0xff).first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
 
     def can(self,permissions):
         return self.role is not None and (self.role.permissions & permissions) == permissions
@@ -126,6 +130,7 @@ class User(UserMixin,db.Model):
         if self.query.filter_by(email=new_email).first() is not None:
             return False
         self.email = new_email
+        self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
         db.session.add(self)
         return True
 
@@ -145,6 +150,14 @@ class User(UserMixin,db.Model):
     # 传入User和password校验password_hash
     def verify_password(self,password):
         return check_password_hash(self.password_hash,password)
+
+    def gravatar(self,size=100,default='identicon',rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = self.avatar_hash
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url,hash=hash,size=size,default=default,rating=rating)
 
     def __repr__(self):
         return '<User %r>' % self.username
